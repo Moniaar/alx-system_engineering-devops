9bc8188cbaa9a4cbb59075f285b9ae221e7c71f8
Moniaar <www.omniaahmed1562001@gmail.com>
2023-12-22 05:55:58 -0800

    102-zombie.c

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Creates an infinite loop.
+ *
+ * Return: Always 0.
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	pid_t child_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+
+		if (child_pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", child_pid);
+			sleep(1);
+		}
+		else if (child_pid == 0)
+		{
+			exit(0);
+		}
+		else
+		{
+			perror("Fork failed");
+			exit(1);
+		}
+	}
+
+	infinite_while();
+
+	return (0);
+}
