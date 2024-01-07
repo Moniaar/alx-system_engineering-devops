68bed83a62dd886d44bea2c9281826746887029c
Moniaar <www.omniaahmed1562001@gmail.com>
2024-01-07 06:06:51 -0800

    Changed the format of the regexp to include everthing that includes the hbtttttn case

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #This script will search for specfic string which is hbtn and multiple of t character
-puts ARGV[0].scan(/(hbt{1,4}n)/).join
+puts ARGV[0].scan(/(hbt{1,5}n)/).join
