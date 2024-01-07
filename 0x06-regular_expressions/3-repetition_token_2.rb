7474ade32205bfe6b8ccd2f1ecf7af67ecc24559
Moniaar <www.omniaahmed1562001@gmail.com>
2024-01-07 06:05:15 -0800

    Changed the format of the regexp to include everthing that includes the t character in a given test cases

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #This script will search for specfic string which is hbtn and multiple of t character
-puts ARGV[0].scan(/t{1,4}|hbtn/).join
+puts ARGV[0].scan(/(hbt{1,4}n)/).join
