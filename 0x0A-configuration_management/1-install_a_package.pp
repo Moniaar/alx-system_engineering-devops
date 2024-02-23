6d56ec44286de9334d912179a9593d7b8e65746c
Moniaar <WWW.omniaahmed1562001@gmail.com>
2024-02-23 21:37:04 +0200

    #4 Installing a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,11 @@
 # Using Puppet, installing flask from pip3
 
-class flask_example {
-  package { 'python3-pip':
-    ensure => installed,
-  }
+package { 'python3-pip':
+  ensure => installed,
+}
 
-  package { 'Flask':
-    ensure   => '2.1.0',
-    provider => 'pip',
-    require  => Package['python3-pip'],
-  }
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
