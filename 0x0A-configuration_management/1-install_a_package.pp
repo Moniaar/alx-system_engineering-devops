ce96f31072af83b4b25ce5cb5407afa44a08ead5
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2024-02-24 00:10:57 +0200

    Using name attribute to specify the package name

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,7 @@
 # Using Puppet, installing flask from pip3
 
-class { 'python':
-  pip => 'ensure => present',
-}
-
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => [Class['python'], Package['pip3']],
+  name     => 'flask',
+  provider => 'pip3'
 }
