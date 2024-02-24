712508fffb25716e48daf53cd355f4dd3eb03117
SolomonChidera <solochidera10@gmail.com>
2024-02-24 08:09:36 +0200

    #7 Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,13 @@
-#!/usr/bin/env bash
+# This is Client configuration file (w/ Puppet)
+
+file { '/home/moniaar/.ssh/config':
+  ensure  => file,
+  mode    => '0600',
+  content => "
+Host your_server_alias  # Provide a meaningful alias for your server
+    HostName 34.224.94.213  # Replace with your server's IP or hostname
+    User ubuntu
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+",
+}
