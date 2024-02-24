5e8f09170ee05d9d29fff11b2392b7bb71539150
SolomonChidera <solochidera10@gmail.com>
2024-02-24 13:29:46 +0200

    #1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,16 @@ package { 'flask':
   provider  =>  'pip3', 
 }
 
+package {'python3-pip':
+  ensure   => installed,
+
+}
 package { 'Werkzeug':
   ensure    =>  '2.1.1',
   provider  =>  'pip3', 
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
 }
