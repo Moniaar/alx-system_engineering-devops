da75d8ffcdd3cc12379e414d29f14e7fc2b94796
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2024-02-24 11:59:26 +0200

    Fixed puppet-lint by adding spaces around =>

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # Using Puppet, installing flask from pip3
 
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3', 
+  ensure    =>  '2.1.0',
+  provider  =>  'pip3', 
 }
 
 package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3'
+  ensure    =>  '2.1.1',
+  provider  =>  'pip3'
 }
