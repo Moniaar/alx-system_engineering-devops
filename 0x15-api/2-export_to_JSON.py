ac40acf3b6b0ea78bb12ed106a5d72483e87ad18
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-25 20:52:18 +0200

    #7 Like task 1 but json format this time

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" a python script that uses a fake online REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
+import re
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    API = "https://jsonplaceholder.typicode.com"
+    if re.fullmatch(r'\d+', argv[1]):
+        id = int(argv[1])
+
+        usr_json = requests.get(f'{API}/users/{id}').json()
+        todos_json = requests.get(f'{API}/todos').json()
+
+        user_name = usr_json.get('username')
+        usr_todos = [task for task in todos_json if task.get('userId') == id]
+
+        with open(f'{id}.json', 'w') as file:
+            data = [{
+                    "task": task.get("title"),
+                    "completed": task.get("completed"),
+                    "username": user_name
+                    } for task in usr_todos
+                    ]
+            user_data = {f'{id}': data}
+            json.dump(user_data, file)
