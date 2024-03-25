0fd11e569c5080843424b376a6770e87258dc6f6
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-25 20:59:29 +0200

    #8 This coding session have been fun

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" a python script that uses a fake online REST API, for a given employee ID,
+returns information about his/her TODO list progress using json diff format now
+"""
+import re
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    API = "https://jsonplaceholder.typicode.com"
+    users_dict = {}
+    users_json = requests.get(f'{API}/users').json()
+    users_id = [user.get("id") for user in users_json]
+
+    for id in users_id:
+        usr_json = requests.get(f'{API}/users/{id}').json()
+        todos_json = requests.get(f'{API}/todos').json()
+
+        user_name = usr_json.get('username')
+        usr_todos = [task for task in todos_json if task.get('userId') == id]
+
+        data = [{
+
+                "username": user_name,
+                "task": task.get("title"),
+                "completed": task.get("completed")
+                } for task in usr_todos
+                ]
+        users_dict[f'{id}'] = data
+
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(users_dict, file)
