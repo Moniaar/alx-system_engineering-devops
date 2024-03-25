59ba221054d8a26066e0d82074930db089d48e11
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-25 21:06:25 +0200

    #11 fixed line 3 importing issue

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ a python script that uses a fake online REST API, for a given employee ID,
-returns information about his/her TODO list progress
-"""
+returns information about his/her TODO list progress in CVS format"""
 import re
 import requests
 from sys import argv
