465feed82c5aedd016d20196523d4348fbb32753
moniaar <www.omniaahmed1562001@gmail.com>
2024-04-10 04:02:54 +0200

    #13 sliced the children list to include only the first 10 posts before iterating over them

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,14 +4,17 @@ import requests
 
 
 def top_ten(subreddit):
-    """a function that queries the Reddit API and prints
+    """A function that queries the Reddit API and prints
     the titles of the first 10 hot posts listed for a given subreddit."""
     url = 'http://www.reddit.com/r/{}/hot/.json'.format(subreddit)
     headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
     params = {'limit': 10}
     response = requests.get(url, headers=headers, params=params)
+
     if response.status_code == 200:
         data = response.json().get('data')
-        [print(child["data"]["title"]) for child in data["children"]]
-        return
-    print('None')
+        children = data.get("children")[:10]
+        for child in children:
+            print(child["data"]["title"])
+    else:
+        print('None')
