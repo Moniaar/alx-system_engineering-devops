1d97e1ff15c9880b951af0588234879c2d5a0fc7
moniaar <www.omniaahmed1562001@gmail.com>
2024-04-10 03:19:36 +0200

    #3 Changed the header parameters to include the project name

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     """This is function that queries the Reddit API
     and returns the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'MyBot/1.0'}
+    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
