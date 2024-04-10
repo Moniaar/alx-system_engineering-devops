38af37a367feef61f16fb35fdf7f3f208430405e
moniaar <www.omniaahmed1562001@gmail.com>
2024-04-10 04:05:01 +0200

    #14 Changed the whole algorithm

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,17 +4,16 @@ import requests
 
 
 def top_ten(subreddit):
-    """A function that queries the Reddit API and prints
-    the titles of the first 10 hot posts listed for a given subreddit."""
+    """A function that queries the Reddit API and prints the
+    titles of the first 10 hot posts listed for a given
+    subreddit
+    """
     url = 'http://www.reddit.com/r/{}/hot/.json'.format(subreddit)
     headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
     params = {'limit': 10}
-    response = requests.get(url, headers=headers, params=params)
-
-    if response.status_code == 200:
-        data = response.json().get('data')
-        children = data.get("children")[:10]
-        for child in children:
-            print(child["data"]["title"])
-    else:
-        print('None')
+    r = requests.get(url, headers=headers, params=params)
+    if r.status_code == 200:
+        data = r.json().get('data')
+        [print(child["data"]["title"]) for child in data["children"]]
+        return
+    print('None')
