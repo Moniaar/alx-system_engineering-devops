4948c3ea6e7dd38987db216027a363731cc72d0f
moniaar <www.omniaahmed1562001@gmail.com>
2024-04-10 03:27:46 +0200

    #7 Changed line 6 and 7 places

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,10 @@ import requests
 def number_of_subscribers(subreddit):
     """This is function that queries the Reddit API
     and returns the number of subscribers"""
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
     if not subreddit or type(subreddit) is not str:
         return 0
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
     r = requests.get(url, headers=headers)
     if r.status_code == 200:
         r = r.json()
