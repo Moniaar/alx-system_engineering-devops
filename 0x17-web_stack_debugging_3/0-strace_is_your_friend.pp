755381b5e999927009f3053f59c2d6eb6aafd233
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2024-04-11 06:51:59 +0200

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,6 @@
 # fixing Apache is returning a 500 error
+
+exec { 'fix_apache_error_config':
+        command  => 'sed -i s/.phpp/.php/g /var/www/html/wp-settings.php',
+        provider => 'shell'
+}
